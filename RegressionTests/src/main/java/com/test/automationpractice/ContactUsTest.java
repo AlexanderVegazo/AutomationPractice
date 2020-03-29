9f004022844184b7cd81f096e770dbc546ed77e1
alexandervegazo <alexander_vicente@hotmail.com>
2020-03-29 17:42:16 -0400

    initial commit contains the modules required for a reusable test automation framework (Page objects, Regression Test and Test Automation Framework).
    Also i added a Run configuration named AmazonBasicTest.
    
    known issue:
    I had a problem in mac to generate maven execution, the systems shows that some packages are not been recognized or imported, but is a apparently is false-positive.

--- RegressionTests/src/main/java/com/test/automationpractice/ContactUsTest.java
+++ RegressionTests/src/main/java/com/test/automationpractice/ContactUsTest.java
@@ -1,4 +1,47 @@
 package com.test.automationpractice;
 
-public class ContactUsTest {
+import com.pageobjects.AutomationPracticePage.ContactUsPage;
+import com.testFramework.core.BaseTest;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ContactUsTest extends BaseTest {
+
+    ContactUsPage contactUsPage;
+
+    @Before
+    public void prepare(){
+        contactUsPage = new ContactUsPage(getDriver());
+    }
+
+    @Test()
+    public void sendAReportToCustomerService(){
+
+        contactUsPage.openContactUsPage();
+        contactUsPage.selectSubjectHeading("Customer service");
+        contactUsPage.setEmailAddress("chalas@gmail.com");
+        contactUsPage.setOrderReference("2020");
+        contactUsPage.setMessage("Tengo un problema");
+        contactUsPage.sendMessageForm();
+        contactUsPage.isMessageSentSuccessfully();
+
+    }
+
+    @Test()
+    public void sendAReportToCustomerServiceWithOutOrderReference(){
+
+        contactUsPage.openContactUsPage();
+        contactUsPage.selectSubjectHeading("Customer service");
+        contactUsPage.setEmailAddress("chalas@gmail.com");
+        contactUsPage.setOrderReference("");
+        contactUsPage.setMessage("Tengo un problema");
+        contactUsPage.sendMessageForm();
+        contactUsPage.isMessageSentSuccessfully();
+
+    }
+
+
+
+
+
 }
